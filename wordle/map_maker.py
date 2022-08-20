7ab35c0529eb6d620fafed6e0b05631420ebc52c
Seth Wenchel <seth@wenchel.com>
2022-08-20 17:20:27 +0000

    updated for a little cleaner guide to guessing

--- wordle/map_maker.py
+++ wordle/map_maker.py
@@ -36,6 +36,28 @@ def get_feedback(word, answer):
     return response
 
 
+def next_wordle(guess, response, wrds):
+    """prints the list of words and the sizes of their largest bins
+    for words that have the biggest median bin size
+
+    Args:
+        guess (str): what you said
+        response (str): what they said ('0' for grey, '1' for yellow, '2' for green, e.g. '00112')
+        wrds (pandas): some big stupid dataframe
+    """
+    bin =0 
+    for idx, i in enumerate(response):
+        bin += int(i)*(3**idx)
+    
+    round2 = wrds.query("words_x==@guess & bin==@bin").copy(deep=True).drop('words_x',1)
+    round2_1 = round2.copy(deep=True)
+    round2 = pd.merge(round2,round2_1, on='bin')
+    round2['bin'] = round2.apply(lambda x: get_feedback(x[0],x[2]), axis=1)
+    pt = round2.pivot_table(index='bin',columns='words_y_x',aggfunc=np.size)
+    ptmd = pt.median()
+    ptmx = pt.max()
+    nxt = ptmx[ptmd==ptmd.max()]
+    print(nxt)
 
 # wrds = pd.read_csv("./words.txt")
 # wrds['key'] = 1
@@ -46,13 +68,5 @@ def get_feedback(word, answer):
 
 # wrds['bin']=wrds.apply(lambda x: get_feedback(x[0],x[1]), axis=1)
 # wrds.to_csv("wrd_outputs.csv")
-wrds = pd.read_csv("wrd_outputs.csv",index_col=0)
-pt = wrds.pivot_table(index='bin',columns='words_x',aggfunc=np.size)
-pt.median()
-
-round2 = wrds.query("words_x=='pique' & bin==54").copy(deep=True).drop('words_x',1)
-round2_1 = round2.copy(deep=True)
-round2 = pd.merge(round2,round2_1, on='bin')
-round2['bin'] = round2.apply(lambda x: get_feedback(x[0],x[2]), axis=1)
-pt = round2.pivot_table(index='bin',columns='words_y_x',aggfunc=np.size)
-meds = pt.median()
+wrds = pd.read_csv("wordle/wrd_outputs.csv",index_col=0)
+next_wordle('pique','00001',wrds)
